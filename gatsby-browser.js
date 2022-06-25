36f5c1d7f5ea287b82884b0c955177ce408c2d06
2357 <61523777+ryota2357@users.noreply.github.com>
2022-06-26 00:23:27 +0900

    global.scss

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import "./src/style/normalize.css";
 
 // custom CSS styles
-import "./src/style/style.scss";
+import "./src/style/global.scss";
 import "./src/style/markdown.scss";
 import "./src/style/code-highlight.scss";
 import "./src/style/anchor.css";
