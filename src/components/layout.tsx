744fd4cfb7fcedc93ae8af1e82c0e25c3f30c617
2357 <61523777+ryota2357@users.noreply.github.com>
2022-06-26 10:42:51 +0900

    refactor: component-layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import "../style/layout.scss"
+import "../style/layout.scss";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { site } = useStaticQuery<Queries.LayoutComponentQuery>(graphql`
@@ -16,55 +16,36 @@ const Layout = ({ children }: { children: ReactNode }) => {
     }
   `);
 
-  const Header = () => (
-    <header>
-      {site?.siteMetadata.title}
-    </header>
-  );
-
-  const Navigation = () => {
-    const List = ({ name, to }: { name: string; to: string }) => (
-      <li style={{ margin: "0 10px" }}>
-        <Link to={to} style={{ color: "#242424" }}>
-          {name}
-        </Link>
-      </li>
-    );
-    return (
-      <nav>
-        <ul>
-          <List name="Home" to="/" key={`/`} />
-          <List name="About" to="/about" key={`about`} />
-          <List name="Blog" to="/blog" key={`blog`} />
-          <List name="GameDev" to="/gamedev" key={`gamedev`} />
-        </ul>
-      </nav>
-    );
-  };
-
-  const Footer = () => (
-    <footer>
-      <p>
-        Built with{" "}
-        <a
-          href="https://www.gatsbyjs.com"
-          style={{ color: "inherit", fontWeight: "bold" }}
-        >
-          Gatsby
-        </a>
-      </p>
-      <p>©2022 {site?.siteMetadata.author.name} All Rights Reserved.</p>
-    </footer>
+  const List = ({ name, to }: { name: string; to: string }) => (
+    <li>
+      <Link to={to}>{name}</Link>
+    </li>
   );
 
   return (
     <div id="layout">
-      <Header />
-      <Navigation />
-      <main>
-        {children}
-      </main>
-      <Footer />
+      <header>{site?.siteMetadata.title}</header>
+      <nav>
+        <ul>
+          <List name="Home" to="/" />
+          <List name="About" to="/about" />
+          <List name="Blog" to="/blog" />
+          <List name="GameDev" to="/gamedev" />
+        </ul>
+      </nav>
+      <main>{children}</main>
+      <footer>
+        <p>
+          Built with{" "}
+          <a
+            href="https://www.gatsbyjs.com"
+            style={{ color: "inherit", fontWeight: "bold" }}
+          >
+            Gatsby
+          </a>
+        </p>
+        <p>©2022 {site?.siteMetadata.author.name} All Rights Reserved.</p>
+      </footer>
     </div>
   );
 };
