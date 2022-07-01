5c4b467522c0bff16a0da454fd6a4805b90d3ad4
2357 <61523777+ryota2357@users.noreply.github.com>
2022-07-01 10:20:35 +0900

    update 404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,10 @@ import "../style/pages/404.scss";
 const NotFoundPage = () => (
   <Layout id="not-found-page">
     <Seo title="404: Not Found" />
-    <h1>404: Not Found</h1>
+    <h1>
+      <span>404</span>
+      <span>Not Found</span>
+    </h1>
   </Layout>
 );
 
