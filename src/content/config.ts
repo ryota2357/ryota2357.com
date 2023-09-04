d9959b5ef796b3be79ac03d5b0ad1ee8e0f854c8
2357 <61523777+ryota2357@users.noreply.github.com>
2023-09-04 11:22:26 +0900

    astro: format

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,32 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     postdate: z.coerce.date(),
     update: z.coerce.date(),
-    tags: z.array(z.string())
-  })
-})
+    tags: z.array(z.string()),
+  }),
+});
 
 const worksCollection = defineCollection({
-  type: 'data',
+  type: "data",
   schema: z.object({
-    data: z.array(z.object({
-      title: z.string(),
-      description: z.string(),
-      url: z.string().url(),
-      image: z.string().startsWith("./"),
-      created: z.coerce.date(),
-      update: z.coerce.date(),
-    }))
-  })
-})
+    data: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        url: z.string().url(),
+        image: z.string().startsWith("./"),
+        created: z.coerce.date(),
+        update: z.coerce.date(),
+      }),
+    ),
+  }),
+});
 
 export const collections = {
-  'blog': blogCollection,
-  'works': worksCollection
-}
+  blog: blogCollection,
+  works: worksCollection,
+};
