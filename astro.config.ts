0a83593152aea331cb97bf77e123f46211072dac
2357 <61523777+ryota2357@users.noreply.github.com>
2023-09-10 00:11:21 +0900

    astro: add sitemap and head

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 // import remarkPrint from "./plugins/remark-print"
 import remarkResolveRelativePageLink from "./plugins/remark-resolve-relative-page-link";
 
 export default defineConfig({
   site: "https://ryota2357.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   markdown: {
     smartypants: false,
     remarkPlugins: [
---
+++ src/pages/sitemap-1.xml.ts
@@ -0,0 +1,69 @@
+import { allBlogCollection, allBlogTagList, util } from "@/content";
+import type { APIContext } from "astro";
+import path from "path";
+
+const xmlHead = [
+  `<?xml version="1.0" encoding="UTF-8"?>`,
+  `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml">`,
+].join("");
+
+const xmlTail = `</urlset>`;
+
+type SitemapItem = {
+  url: URL;
+  lastmod?: Date;
+  changefreq?:
+    | "daily"
+    | "monthly"
+    | "always"
+    | "hourly"
+    | "weekly"
+    | "yearly"
+    | "never";
+  priority?: number;
+};
+
+export function get(context: APIContext) {
+  const site = context.site?.origin; // Astro.site is not defined here.
+
+  const blogPost: SitemapItem[] = allBlogCollection.map((post) => ({
+    url: new URL(path.join("/blog/", post.slug, "/"), site),
+    lastmod: post.data.update,
+    priority: 0.7,
+  }));
+
+  const blogTag: SitemapItem[] = allBlogTagList.map((tag) => ({
+    url: new URL(path.join("/blog/tag/", tag.name, "/"), site),
+    priority: 0.3,
+  }));
+
+  const blogYear: SitemapItem[] = allBlogCollection
+    .map((post) => util.getYearFromSlug(post.slug))
+    .filter((year, index, self) => self.indexOf(year) === index)
+    .map((year) => ({
+      url: new URL(path.join("/blog/", year, "/"), site),
+      priority: 0.3,
+    }));
+
+  return {
+    body: `${xmlHead}\n${blogPost
+      .concat(...blogTag)
+      .concat(...blogYear)
+      .map((item) =>
+        [
+          `<url>`,
+          `<loc>${item.url.toString()}</loc>`,
+          item.lastmod
+            ? `<lastmod>${item.lastmod.toISOString()}</lastmod>`
+            : undefined,
+          item.changefreq
+            ? `<changefreq>${item.changefreq}</changefreq>`
+            : undefined,
+          item.priority ? `<priority>${item.priority}</priority>` : undefined,
+          `</url>`,
+        ].filter((x) => x !== undefined),
+      )
+      .map((taggedItems) => taggedItems.join(""))
+      .join("\n")}\n${xmlTail}`,
+  };
+}
