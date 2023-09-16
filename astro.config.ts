71a4c7677f5cb66028f121a33021f1f11ab413ca
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2023-09-16 16:30:21 +0900

    feat: add gtag

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // import remarkPrint from "./plugins/remark-print"
 import remarkResolveRelativePageLink from "./plugins/remark-resolve-relative-page-link";
 
 export default defineConfig({
   site: "https://ryota2357.com",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     smartypants: false,
     remarkPlugins: [
