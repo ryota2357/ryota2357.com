f6df5e4b4bba57bc95fb04b2f9b6ed4811e8b4b1
2357 <61523777+ryota2357@users.noreply.github.com>
2023-09-29 14:58:42 +0900

    fix: type error

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,22 @@
-import { APIContext } from "astro"
-import rss from "@astrojs/rss"
-import { allBlogCollection } from "@/content"
-import path from "path"
+import type { APIContext } from "astro";
+import rss from "@astrojs/rss";
+import { allBlogCollection } from "@/content";
+import path from "path";
 
 export function GET(context: APIContext) {
+  if (!context.site) {
+    throw new Error("You need to set `site` in astro.config");
+  }
   return rss({
     title: "ryota2357.com",
     description: "ryota2357's homepage and blog",
     site: context.site,
-    items: allBlogCollection.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.postdate,
-      link: new URL(path.join("/blog/", post.slug), context.site).pathname,
-    })).sort((a, b) => b.pubDate - a.pubDate),
-  })
+    items: allBlogCollection
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.postdate,
+        link: new URL(path.join("/blog/", post.slug), context.site).pathname,
+      }))
+      .sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf()),
+  });
 }
