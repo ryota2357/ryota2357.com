d47a779631b8bc253ca29fc8bb3d38b7c27f0458
GitHub <noreply@github.com>
2024-03-31 01:04:38 +0000

    Update deno dependencies

--- util/generate_simple_html_files/main.ts
+++ util/generate_simple_html_files/main.ts
@@ -2,8 +2,8 @@ import {
   copy,
   exists,
   expandGlob,
-} from "https://deno.land/std@0.216.0/fs/mod.ts";
-import { dirname, join } from "https://deno.land/std@0.216.0/path/mod.ts";
+} from "https://deno.land/std@0.221.0/fs/mod.ts";
+import { dirname, join } from "https://deno.land/std@0.221.0/path/mod.ts";
 
 // /myPath/index.html => /myPath.html
 async function generateSimpleHtmlFiles(outDir: URL) {
