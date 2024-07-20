613393d54a49f2df2abb85f684337b203ea4fadb
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-07-21 02:07:01 +0900

    chore: not create deno lockfile

--- util/generate_simple_html_files.sh
+++ util/generate_simple_html_files.sh
@@ -2,7 +2,7 @@
 
 if type deno > /dev/null 2>&1; then
     cd "$(dirname "$0")/generate_simple_html_files" || exit 1
-    deno run --allow-read --allow-write main.ts
+    deno run --allow-read --allow-write --no-lock main.ts
 else
     echo "deno is not installed."
     exit 1
