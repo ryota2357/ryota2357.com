a160a3d9b8e8f162e26ea5bc1208a46d0584ff77
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-10-03 21:22:57 +0900

    chore: use direnv and nix for development

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_20
+            pnpm
+            astro-language-server
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
