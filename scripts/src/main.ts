da92108ef1c5f97e37f677770a9df55f60731652
GitHub <noreply@github.com>
2025-07-06 01:07:20 +0000

    Update deno dependencies

--- scripts/src/main.ts
+++ scripts/src/main.ts
@@ -1,5 +1,5 @@
-import { copy, exists, expandGlob } from "jsr:@std/fs@1.0.18";
-import { dirname, join } from "jsr:@std/path@1.1.0";
+import { copy, exists, expandGlob } from "jsr:@std/fs@1.0.19";
+import { dirname, join } from "jsr:@std/path@1.1.1";
 
 // /myPath/index.html => /myPath.html
 async function generateSimpleHtmlFiles(outDir: URL) {
